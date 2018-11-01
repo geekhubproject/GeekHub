cddf3b5cad06c18a1e72e7482fdc86707fa6dbe2
appu7 <appuappu811@gmail.com>
2018-11-02 00:08:47 +0530

    enabled cors

--- node/app.js
+++ node/app.js
@@ -1,20 +1,22 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./modules/stack-overflow/routes');
-var gitRouter = require('./modules/github/routes');
-var mediumRouter = require('./modules/medium/routes');
-var userRouter = require('./modules/user/routes');
+const indexRouter = require('./modules/stack-overflow/routes');
+const gitRouter = require('./modules/github/routes');
+const mediumRouter = require('./modules/medium/routes');
+const userRouter = require('./modules/user/routes');
 
 const mongo = require('./models/mongodb/connect');
 const passportLocal = require('./modules/common/auth/passport-local');
 const passportJWT = require('./modules/common/auth/passport-jwt');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
