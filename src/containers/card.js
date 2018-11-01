0215a47fe8672814e4349507dcb2e73ed641c32a
appu7 <appuappu811@gmail.com>
2018-11-02 01:11:34 +0530

    updated anchor style

--- src/containers/card.js
+++ src/containers/card.js
@@ -10,23 +10,24 @@ import Typography from "@material-ui/core/Typography";
 // Media card Functional Component
 function MediaCard(props) {
 	return (
-		<Card style={{width:"100%"}}>
-			<CardActionArea style={{width:"100%"}}>
-				<CardContent >
-					<Typography align="left" gutterBottom variant="h5" component="h2"
-											onClick={() => window.location.assign(props.link)}
-					>
-						{props.title}
-					</Typography>
-					<Typography align="left" component="p">
-						{props.description}
-					</Typography>
-				</CardContent>
-			</CardActionArea>
-			<CardActions>
-				{props.stats}
-			</CardActions>
-		</Card>
+		<a href={props.link} className="remove-anchor">
+			<Card style={{width:"100%"}}>
+				<CardActionArea style={{width:"100%"}}>
+					<CardContent >
+						<Typography align="left" gutterBottom variant="h5" component="h2"
+						>
+							{props.title}
+						</Typography>
+						<Typography align="left" component="p">
+							{props.description}
+						</Typography>
+					</CardContent>
+				</CardActionArea>
+				<CardActions>
+					{props.stats}
+				</CardActions>
+			</Card>
+		</a>
 	);
 }
 
