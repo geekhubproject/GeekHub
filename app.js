dfe66d9ee9671a4f4df97076ede623b9ce8842d8
appu7 <appuappu811@gmail.com>
2018-12-09 03:18:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(session({
 }));
 
 app.use(logger('dev'));
-app.use(cors({origin: 'geek-hub.herokuapp.com/:1', credentials: true}));
+app.use(cors({origin: 'geek-hub.herokuapp.com', credentials: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
