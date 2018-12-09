c9f7689eaf2e6f430207ce2e7633e5f49d0ffd6f
ajith.a <ajith.a@mahindacomviva.com>
2018-12-10 00:42:42 +0530

    Rebuilt
    Signed-off-by: ajith.a <ajith.a@mahindacomviva.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -117,9 +117,4 @@ export default {
   .fr-wrapper > div:first-child {
     display: none;
   }
-  a[href="https://froala.com/wysiwyg-editor"], a[href="https://www.froala.com/wysiwyg-editor?k=u"] {
-    display: none !important;
-    position: absolute;
-    top: -99999999px;
-  }
 </style>
