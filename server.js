5a77e3fc7c870ee98531ce01d1a8b214b6856c73
ajith-anburaj <appuappu811@gmail.com>
2019-11-05 16:18:42 +0530

    return app on every request

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const serveStatic = require('serve-static');
 
 let app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.get('*', serveStatic(__dirname + "/dist/index.html"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
